71d610b22d40176caa8dc191f38ebbcb30a67bd1
tugilane <marten.peljo@gmail.com>
2025-12-12 20:09:56 +0200

    very simple contact us page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../views/LandingPage.vue'
 import SignupPage from '../views/SignupPage.vue'
+import ContactUsPage from '../views/ContactUsPage.vue'
 
 const routes = [
   { path: '/', component: LandingPage },
-  { path: '/signup', component: SignupPage }
+  { path: '/signup', component: SignupPage },
+  { path: '/contact', component: ContactUsPage }
 ]
 
 export default createRouter({
---
+++ src/views/ContactUsPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h1>Contact us</h1>
+    <p>Contact us at support@TeamL.com</p>
+    <p>Office at California state, Json avenue 41</p>
+  </section>
+</template>
